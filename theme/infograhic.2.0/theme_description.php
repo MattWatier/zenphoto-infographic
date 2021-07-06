d8bfa92fa953d3c4f6b18cdea6ea8dfb003603d6
Matt Watier <mattwatier@gmail.com>
2021-07-06 09:51:18 -0400

    text edits

--- theme/infograhic.2.0/theme_description.php
+++ theme/infograhic.2.0/theme_description.php
@@ -1,6 +1,6 @@
 <?php
 // Zenphoto theme definition file
-$theme_description['name'] = 'Matt\'s Masonary Theme with infographic V.6. Zenphoto 1.4.5version';
+$theme_description['name'] = 'Matt\'s Masonary Theme with infographic V.6. Zenphoto 1.4.5 version';
 $theme_description['author'] = gettext('<a href="http://mattwatier.com" target="_blank">Matt Watier</a>');
 $theme_description['version'] = '6.1.1';
 $theme_description['date'] = '3/13/2013';
